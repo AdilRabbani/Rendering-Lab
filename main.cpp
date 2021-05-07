f4473e87bd7aa86f29462f009c70842e392ddda5
Adil <adil.rabbani@fifty2.eu>
2021-05-07 05:38:02 +0200

    Section 2: Creating a spectrum

--- main.cpp
+++ main.cpp
@@ -4,20 +4,25 @@ int main() {
 
     // Image
 
-    const int image_width = 3;
-    const int image_height = 3;
+    const int image_width = 10;
+    const int image_height = 10;
 
     // Render
 
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    std::cout << 255 << ' ' << 0 << ' ' << 0 << '\n';
-    std::cout << 0 << ' ' << 255 << ' ' << 0 << '\n';
-    std::cout << 0 << ' ' << 0 << ' ' << 0 << '\n';
-    std::cout << 0 << ' ' << 0 << ' ' << 255 << '\n';
-    std::cout << 255 << ' ' << 255 << ' ' << 0 << '\n';
-    std::cout << 255 << ' ' << 0 << ' ' << 255 << '\n';
-    std::cout << 255 << ' ' << 255 << ' ' << 255 << '\n';
-    std::cout << 0 << ' ' << 255 << ' ' << 255 << '\n';
-    std::cout << 255 << ' ' << 100 << ' ' << 0 << '\n';
+    for (int j = image_height-1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto r = double(i) / (image_width-1);
+            auto g = double(j) / (image_height-1);
+            auto b = 0;
+
+            int ir = static_cast<int>(255.999 * r);
+            int ig = static_cast<int>(255.999 * g);
+            int ib = static_cast<int>(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
 }
